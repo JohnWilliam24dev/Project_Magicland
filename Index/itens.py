35633ca0c62228cc402119f5cacf5a9be6b4cf44
JohnWilliam24dev <163377183+JohnWilliam24dev@users.noreply.github.com>
2024-03-21 19:05:32 -0300

    backup

---
+++ Index/itens.py
@@ -0,0 +1,10 @@
+import atribute
+class Itens():
+
+    def __init__(self,name,description):
+        self.name=name
+        self.description=description
+        self.active=[True,False]
+item1=Itens("Matadora de dragões","este itemé um item de dano fisico que blblblb")
+print(item1)
+        
